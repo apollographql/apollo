109aa36d0d7489590fad4c71172ff07226219d97
Loren Sands-Ramshaw <lorensr@gmail.com>
2016-07-12 11:04:11 -0400

    event -> e, syntax (fixes #52)

--- js/main.js
+++ js/main.js
@@ -24,9 +24,9 @@ $(function() {
   });
 
   $('#newsletter-form').submit(function(e) {
-    e.preventDefault()
-    var email = event.target.email.value;
-    var latestformsubmit = 'Apollo Newsletter Subscription'
+    e.preventDefault();
+    var email = e.target.email.value;
+    var latestformsubmit = 'Apollo Newsletter Subscription';
     analytics.identify(email, {email: email, LatestFormSubmit: latestformsubmit});
     analytics.track('web.apollo-newsletter');
     $('.newsletter-form').addClass('confirmed');
@@ -34,13 +34,13 @@ $(function() {
     setTimeout(function(){
       $('.layout').attr('class', 'layout');
     }, 1200);
-  })
+  });
 
   $('#slack-form').submit(function(e) {
-    e.preventDefault()
-    var email = event.target.email.value;
-    var latestformsubmit = 'Apollo Slack Signup'
-    var inviteuri = "https://29268947-a94c-4d45-baa2-9641a9848cad.trayapp.io?email=" + email
+    e.preventDefault();
+    var email = e.target.email.value;
+    var latestformsubmit = 'Apollo Slack Signup';
+    var inviteuri = "https://29268947-a94c-4d45-baa2-9641a9848cad.trayapp.io?email=" + email;
     $.get(inviteuri);
     analytics.identify(email, {email: email, LatestFormSubmit: latestformsubmit});
     analytics.track('web.apollo-slack');
@@ -49,20 +49,19 @@ $(function() {
     setTimeout(function(){
       $('.layout').attr('class', 'layout');
     }, 1200);
-  })
+  });
 
   $('#consultation-form').submit(function(e) {
-    e.preventDefault()
-    var email = event.target.email.value;
-    var firstname = event.target.firstname.value;
-    var lastname = event.target.lastname.value;
-    var company = event.target.company.value;
-    var message = event.target.message.value;
-    var latestformsubmit = 'Apollo Developer Support'
+    e.preventDefault();
+    var email = e.target.email.value;
+    var firstname = e.target.firstname.value;
+    var lastname = e.target.lastname.value;
+    var company = e.target.company.value;
+    var message = e.target.message.value;
+    var latestformsubmit = 'Apollo Developer Support';
 
     analytics.identify(email, {email: email, firstName: firstname, lastName: lastname, company: company, Message__c: message, LatestFormSubmit: latestformsubmit});
     $('.consultation-form').addClass('confirmed');
     analytics.track('web.apollo-devsub');
-  })
-}
-);
+  });
+});
