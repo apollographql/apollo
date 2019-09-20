06ca58789b8afa0f55d63a76f18504562164a161
Trevor Blades <tdblades@gmail.com>
2019-09-20 15:11:11 -0700

    Fine-tune and share styles

--- docs/source/components/docset-menu.js
+++ docs/source/components/docset-menu.js
@@ -1,12 +1,16 @@
 import React, { useContext } from 'react';
 import styled from '@emotion/styled';
 import { IconClients } from '@apollo/space-kit/icons/IconClients';
-import { colors, breakpoints } from 'gatsby-theme-apollo-core';
+import {
+  NavItemsContext,
+  NavItemTitle,
+  NavItemDescription
+} from 'gatsby-theme-apollo-docs';
+import { breakpoints, colors } from 'gatsby-theme-apollo-core';
 import { colors as spaceKitColors } from '@apollo/space-kit/colors';
 import { size } from 'polished';
-import { NavItemsContext } from 'gatsby-theme-apollo-docs/src/components/page-layout';
 
-const spacing = 8;
+const spacing = 12;
 const Wrapper = styled.div({
   display: 'flex',
   flexWrap: 'wrap',
@@ -49,10 +53,8 @@ const IconWrapper = styled.div({
   }
 });
 
-const Title = styled.h4({
-  fontWeight: 600,
-  lineHeight: 1.3,
-  marginBottom: 8
+const TextWrapper = styled.div({
+  color: colors.text1
 });
 
 const StyledLink = styled.a({
@@ -61,10 +63,6 @@ const StyledLink = styled.a({
   ':hover': {
     textDecoration: 'underline'
   }
-});
-
-const Description = styled.h6({
-  color: colors.text3
 });
 
 export default function DocsetMenu() {
@@ -76,14 +74,14 @@ export default function DocsetMenu() {
           <IconWrapper>
             <IconClients />
           </IconWrapper>
-          <div>
-            <Title>
+          <TextWrapper>
+            <NavItemTitle>
               <StyledLink href={navItem.url}>
                 {navItem.title}
               </StyledLink>
-            </Title>
-            <Description>{navItem.description}</Description>
-          </div>
+            </NavItemTitle>
+            <NavItemDescription>{navItem.description}</NavItemDescription>
+          </TextWrapper>
         </MenuItem>
       ))}
     </Wrapper>
