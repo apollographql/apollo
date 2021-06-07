1737ab2dda4faed3eabfef614fdcb0b1bbdd0163
Trevor Blades <tdblades@gmail.com>
2021-06-07 13:21:55 -0700

    Run merge function locally and make some code adjustments

--- merge.js
+++ merge.js
@@ -1,12 +1,8 @@
 const algoliasearch = require('algoliasearch');
 const {chunk} = require('lodash');
 
-const client = algoliasearch(
-  process.env.ALGOLIA_APP_ID,
-  process.env.ALGOLIA_API_KEY
-);
-
-const docsets = [
+const CHUNK_SIZE = 1000;
+const DOCSETS = [
   'rover',
   'studio',
   'federation',
@@ -16,13 +12,16 @@ const docsets = [
   'server'
 ];
 
+const client = algoliasearch(
+  process.env.ALGOLIA_APP_ID,
+  process.env.ALGOLIA_API_KEY
+);
+
 const mainIndex = client.initIndex('docs');
-const subIndices = docsets.map(docset => client.initIndex(docset));
+const subIndices = DOCSETS.map(docset => client.initIndex(docset));
 
 async function fetchAll() {
-  const objs = await Promise.all(
-    subIndices.map(async theIndex => fetch(theIndex))
-  );
+  const objs = await Promise.all(subIndices.map(fetch));
   return objs.flat();
 }
 
@@ -39,20 +38,24 @@ async function fetch(index) {
 
 async function sendAll() {
   const allObjs = await fetchAll();
-  console.log(allObjs.length);
-  const chunkSize = 1000;
-  const chunks = chunk(allObjs, chunkSize);
+  const chunks = chunk(allObjs, CHUNK_SIZE);
 
-  mainIndex.saveObjects(allObjs);
-
-  const chunkJobs = chunks.map(async function (chunked) {
-    return mainIndex.saveObjects(chunked).wait();
+  // mainIndex.saveObjects(allObjs);
+  const jobs = chunks.map(async (chunk, index, array) => {
+    const xOfY = `${index + 1} of ${array.length}`;
+    console.log(`Saving chunk ${xOfY}`);
+    await mainIndex.saveObjects(chunk).wait();
+    console.log(`Chunk ${xOfY} saved`);
+    return chunk.length;
   });
 
-  await Promise.all(chunkJobs);
+  return Promise.all(jobs);
 }
 
-sendAll();
+sendAll().then(results => {
+  const totalObjs = results.reduce((acc, result) => acc + result, 0);
+  console.log('Docs index merge complete. %s objects merged.', totalObjs);
+});
 
 // async function moveIndex(client, sourceIndex, targetIndex) {
 //   const {taskID} = await client.moveIndex(
