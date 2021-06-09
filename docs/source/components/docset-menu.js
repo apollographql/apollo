c090b5838bf77bc3c8fdb4b5b568acfa2a1299a4
Trevor Blades <tdblades@gmail.com>
2021-06-09 16:59:54 -0700

    Use DocsetIcon from autocomplete

--- docs/source/components/docset-menu.js
+++ docs/source/components/docset-menu.js
@@ -1,44 +1,8 @@
 import React, { useContext } from 'react';
 import styled from '@emotion/styled';
-import { ReactComponent as AppleLogo } from '../assets/apple-logo.svg';
-import { ReactComponent as AndroidLogo } from '../assets/android-logo.svg';
-import { ReactComponent as FederationIcon } from '../assets/federation.svg';
-import { IconSatellite3 } from '@apollo/space-kit/icons/IconSatellite3';
-import { IconSchema } from '@apollo/space-kit/icons/IconSchema';
-import { ReactComponent as ReactLogo } from '../assets/react-logo.svg';
-import { ReactComponent as RoverIcon } from '../assets/rover-logo.svg';
-import {
-  NavItemsContext,
-  NavItemDescription
-} from 'gatsby-theme-apollo-docs';
-import { colors } from '@apollo/space-kit/colors';
-import { size } from 'polished';
-import { MenuWrapper, MenuItem } from './menu';
-
-function getBoxShadow(opacity, y, blur) {
-  return `rgba(18, 21, 26, ${opacity}) 0 ${y}px ${blur}px`
-}
-
-const {indigo} = colors;
-const AppIcon = styled.div({
-  ...size('100%'),
-  display: 'flex',
-  alignItems: 'center',
-  justifyContent: 'center',
-  boxShadow: [
-    getBoxShadow(0.12, 1, 2),
-    getBoxShadow(0.1, 2, 4),
-    getBoxShadow(0.08, 5, 10),
-    `inset rgba(45, 31, 102, 0.4) 0 -1px 2px`
-  ].toString(),
-  borderRadius: 8,
-  color: indigo.lighter,
-  backgroundImage: `linear-gradient(${[indigo.base, indigo.dark]})`,
-  svg: {
-    ...size(16),
-    fill: 'currentColor'
-  }
-});
+import { DocsetIcon } from 'apollo-algolia-autocomplete';
+import { MenuItem, MenuWrapper } from './menu';
+import { NavItemDescription, NavItemsContext } from 'gatsby-theme-apollo-docs';
 
 const StyledLink = styled.a({
   color: 'inherit',
@@ -48,35 +12,17 @@ const StyledLink = styled.a({
   }
 });
 
-const icons = [
-  <ReactLogo />,
-  <AppleLogo style={{paddingBottom: 1}} />,
-  <AndroidLogo />,
-  <IconSatellite3 weight="thin" />,
-  <FederationIcon />,
-  <IconSchema weight="thin" />,
-  <RoverIcon />
-];
-
 export default function DocsetMenu() {
   const navItems = useContext(NavItemsContext);
   return (
     <MenuWrapper>
       {navItems
-        .filter((navItem) => !navItem.omitLandingPage)
+        .filter(navItem => !navItem.omitLandingPage)
         .map((navItem, index) => (
           <MenuItem
             key={navItem.url}
-            icon={(
-              <AppIcon>
-                {icons[index]}
-              </AppIcon>
-            )}
-            title={(
-              <StyledLink href={navItem.url}>
-                {navItem.title}
-              </StyledLink>
-            )}
+            icon={<DocsetIcon docset={navItem.docset} />}
+            title={<StyledLink href={navItem.url}>{navItem.title}</StyledLink>}
           >
             <NavItemDescription>{navItem.description}</NavItemDescription>
           </MenuItem>
