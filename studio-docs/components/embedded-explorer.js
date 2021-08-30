fcbad100624c56e18bd7a8e0b446e739fe540bdd
mayakoneval <mayakoneval@gmail.com>
2021-08-30 09:29:29 -0700

    update embeddable explorer component to use full stack demo

--- studio-docs/components/embedded-explorer.js
+++ studio-docs/components/embedded-explorer.js
@@ -1,6 +1,7 @@
-import React, { useEffect } from 'react';
+import React, { useEffect } from "react";
 
-const EMBEDDABLE_EXPLORER_URL = 'https://embed.apollo.local:3000/?graphRef=acephei@current&docsPanelState=closed';
+const EMBEDDABLE_EXPLORER_URL =
+  "https://explorer.embed.apollographql.com/?graphRef=Apollo-Fullstack-Demo-o3tsz8@current&docsPanelState=closed";
 
 async function executeOperation({
   operation,
@@ -10,58 +11,72 @@ async function executeOperation({
 }) {
   if (
     headers &&
-    Object.keys(headers).every(
-      (key) => key.toLowerCase() !== 'content-type',
-    )
+    Object.keys(headers).every((key) => key.toLowerCase() !== "content-type")
   ) {
-    headers['content-type'] = 'application/json';
+    headers["content-type"] = "application/json";
   }
   const response = await fetch(
-    "https://acephei-gateway.herokuapp.com",
+    "https://apollo-fullstack-tutorial.herokuapp.com/",
     {
-      method: 'POST',
+      method: "POST",
       headers,
       body: JSON.stringify({
         query: operation,
         variables,
         operationName,
       }),
-    },
-  )
+    }
+  );
   return await response.json();
 }
 
 export const EmbeddedExplorer = () => {
   useEffect(() => {
     const onPostMessageReceived = (event) => {
-      const isQueryOrMutation = event.data.name?.startsWith('ExplorerRequest:');
-      const isSubscription = event.data.name?.startsWith('ExplorerSubscriptionRequest:');
-      const currentOperationId = event.data.name?.split(':')[1]
-      if(isQueryOrMutation || isSubscription) {
-        if(event.data.operation){
-          executeOperation({
-            operation: event.data.operation,
-            operationName: event.data.operationName,
-            variables: event.data.variables,
-            headers: event.data.headers,
-          }).then((response) => {
-            const embeddedExplorerIFrame = document.getElementById('embedded-explorer');
-            embeddedExplorerIFrame.contentWindow.postMessage({
-              name: isQueryOrMutation ?
-                `ExplorerResponse:${currentOperationId}` :
-                `ExplorerSubscriptionResponse:${currentOperationId}`,
-              response,
-            }, EMBEDDABLE_EXPLORER_URL);
-          });
-        }
+      const isQueryOrMutation = event.data.name?.startsWith("ExplorerRequest:");
+      const isSubscription = event.data.name?.startsWith(
+        "ExplorerSubscriptionRequest:"
+      );
+      const currentOperationId = event.data.name?.split(":")[1];
+      if ((isQueryOrMutation || isSubscription) && event.data.operation) {
+        const { operation, operationName, variables, headers } = event.data;
+        executeOperation({
+          operation,
+          operationName,
+          variables,
+          headers,
+        }).then((response) => {
+          document
+            .getElementById("embedded-explorer")
+            ?.contentWindow.postMessage(
+              {
+                name: isQueryOrMutation
+                  ? `ExplorerResponse:${currentOperationId}`
+                  : `ExplorerSubscriptionResponse:${currentOperationId}`,
+                response,
+              },
+              EMBEDDABLE_EXPLORER_URL
+            );
+        });
       }
-    }
-    window.addEventListener('message', onPostMessageReceived);
+    };
+    window.addEventListener("message", onPostMessageReceived);
 
-    return () => window.removeEventListener('message', onPostMessageReceived);
-  }, [])
+    return () => window.removeEventListener("message", onPostMessageReceived);
+  }, []);
 
   return (
-      <iframe id="embedded-explorer" style={{width: '100%', height: 500, border: 0, borderRadius: 4, boxShadow: '0 4px 8px 0 rgba(0,0,0,0.04)'}} title="embedded-explorer" src={EMBEDDABLE_EXPLORER_URL}/>
+    <iframe
+      id="embedded-explorer"
+      style={{
+        width: "100%",
+        height: 500,
+        border: 0,
+        borderRadius: 4,
+        boxShadow: "0 4px 8px 0 rgba(0,0,0,0.04)",
+      }}
+      title="embedded-explorer"
+      src={EMBEDDABLE_EXPLORER_URL}
+    />
   );
-}
+};
