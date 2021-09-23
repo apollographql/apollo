fd6dc9dfe8834bdeac09f5d483d31b72ce4514fd
Stephen Barlow <stephen@apollographql.com>
2021-09-23 16:07:18 -0700

    Add top articles below every homepage docset (#1234)
    * Add top articles below every homepage docset
    
    * Fix some link colors
    
    * Shrink the homepage buttons a bit

--- docs/source/components/docset-menu.js
+++ docs/source/components/docset-menu.js
@@ -3,12 +3,38 @@ import styled from '@emotion/styled';
 import { DocsetIcon } from 'apollo-algolia-autocomplete';
 import { MenuItem, MenuWrapper } from './menu';
 import { NavItemDescription, NavItemsContext } from 'gatsby-theme-apollo-docs';
+import { colors } from 'gatsby-theme-apollo-core';
 import './styles.css'
 const StyledLink = styled.a({
   color: 'inherit',
   textDecoration: 'none',
   ':hover': {
     textDecoration: 'underline'
+  }
+});
+
+const DocsetDescription = styled.p({
+  fontSize: '14px',
+  marginBottom: '5px'
+});
+
+const ArticleList = styled.ul({
+  marginBottom: 0,
+});
+
+const ArticleListItem = styled.li({
+  fontSize: '14px',
+  marginBottom: 0,
+});
+
+const ArticleListLink = styled.a({
+  color: colors.primary,
+  textDecoration: 'none',
+  ':hover': {
+    textDecoration: 'underline'
+  },
+  ':visited': {
+    color: '#3f20ba'
   }
 });
 
@@ -23,8 +49,16 @@ export default function DocsetMenu() {
             key={navItem.url}
             icon={<DocsetIcon docset={navItem.docset === 'apollo-server' ? 'server' : navItem.docset} />}
             title={<StyledLink href={navItem.url}>{navItem.title}</StyledLink>}
+            style={{"marginBottom": "20px"}}
           >
-            <NavItemDescription>{navItem.description}</NavItemDescription>
+            <DocsetDescription>{navItem.description}</DocsetDescription>
+            <ArticleList>
+              {navItem.topArticles?.map((article) => (
+                <ArticleListItem>
+                  <ArticleListLink href={article.url}>{article.title}</ArticleListLink>
+                </ArticleListItem>
+              ))}
+            </ArticleList>
           </MenuItem>
         ))}
     </MenuWrapper>
